4f773f161c04cd816582da989ad476b20effd248
Kauã Mendes <77807734+Kauamendes@users.noreply.github.com>
2023-11-01 21:14:51 -0300

    Add files via upload

---
+++ src/main/java/com/example/demo/domain/Bairro.java
@@ -0,0 +1,12 @@
+package com.example.demo.domain;
+
+import lombok.*;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@EqualsAndHashCode(of = "id")
+public class Bairro {
+}
---
+++ src/main/java/com/example/demo/domain/Usuario.java
@@ -0,0 +1,17 @@
+package com.example.demo.domain;
+
+import lombok.*;
+
+import java.util.List;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@EqualsAndHashCode(of = "id")
+public class Usuario {
+
+
+
+}
