7bc9a0df2dd3d4368a68af1cfc72572b1c326328
KauaMendes <kauamendes80@hotmail.com>
2023-11-14 20:57:12 -0300

    ajustes

--- src/main/java/com/example/demo/controller/RelatorioController.java
+++ src/main/java/com/example/demo/controller/RelatorioController.java
@@ -1,9 +1,26 @@
 package com.example.demo.controller;
 
+import com.example.demo.repository.RelatorioRepository;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping("/relatorios")
 public class RelatorioController {
+
+    private RelatorioRepository relatorioRepository;
+
+    @GetMapping("/")
+    public String paginaInicial() {
+        return "relatorio";
+    }
+
+//    @GetMapping("/buscar")
+//    public ModelAndView buscar(RelatorioDto relatorioDto) throws SQLException {
+//        ModelAndView mv = new ModelAndView("relatorio");
+//        mv.addObject("vacinaBairro", relatorioRepository.buscar(relatorioDto));
+//        return mv;
+//    }
+
 }
