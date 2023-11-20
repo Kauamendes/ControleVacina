3fc1c2b98bed176ea0864b3263bf036d443be6cf
Rafael-Floriano <rafael.florianoc9@gmail.com>
2023-11-20 20:44:19 -0300

    prototipo query

--- src/main/java/com/example/demo/repository/RelatorioRepository.java
+++ src/main/java/com/example/demo/repository/RelatorioRepository.java
@@ -1,9 +1,11 @@
 package com.example.demo.repository;
 
+import com.example.demo.config.Conexao;
 import com.example.demo.dto.RelatorioDto;
 import com.example.demo.dto.VacinaBairroDto;
 import org.springframework.stereotype.Repository;
 
+import java.sql.Connection;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -11,6 +13,16 @@ import java.util.List;
 public class RelatorioRepository {
     public List<VacinaBairroDto> buscar(RelatorioDto relatorioDto) {
         List<VacinaBairroDto> vacinas = new ArrayList<>();
+        Conexao conexao = new Conexao();
+        Connection conn = conexao.conectar();
+
+        String query = " SELECT a.id as id_vacina, a.nome as nome_vacina, " +
+                " c.nome as bairro_nome, b.data_aplicacao " +
+                " FROM vacina a " +
+                " INNER JOIN vacina_bairro b ON b.vacina_id = a.id " +
+                " INNER JOIN bairro c ON c.id = b.bairro_id ";
+
+
         return vacinas;
     }
 }
