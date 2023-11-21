401eb7141713eb1563e1d459279e7a43ffc71230
Rafael-Floriano <rafael.florianoc9@gmail.com>
2023-11-21 20:53:24 -0300

    fixed query of 'vacinas' report

--- src/main/java/com/example/demo/dto/VacinaBairroDto.java
+++ src/main/java/com/example/demo/dto/VacinaBairroDto.java
@@ -1,10 +1,12 @@
 package com.example.demo.dto;
 
+import lombok.Builder;
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
+@Builder
 public class VacinaBairroDto {
 
     private Long id;
--- src/main/java/com/example/demo/repository/RelatorioRepository.java
+++ src/main/java/com/example/demo/repository/RelatorioRepository.java
@@ -3,52 +3,67 @@ package com.example.demo.repository;
 import com.example.demo.config.Conexao;
 import com.example.demo.dto.RelatorioDto;
 import com.example.demo.dto.VacinaBairroDto;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
 import org.springframework.stereotype.Repository;
 
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.sql.SQLException;
+import javax.sql.DataSource;
+import java.sql.*;
 import java.util.ArrayList;
 import java.util.List;
 
 @Repository
 public class RelatorioRepository {
-    //List<VacinaBairroDto>
-    public String buscar(RelatorioDto relatorioDto) {
+
+    public List<VacinaBairroDto> buscar(RelatorioDto relatorioDto) throws SQLException {
         List<VacinaBairroDto> vacinas = new ArrayList<>();
-        Conexao conexao = new Conexao();
-        Connection conn = conexao.conectar();
-        String resultado = "";
 
         if (relatorioDto.getBairro().isBlank()) {
             relatorioDto.setBairro(null);
         }
-        if (relatorioDto.getDataInicio().isBlank()) {
-            relatorioDto.setDataInicio(null);
-        }
-        if (relatorioDto.getDataFim().isBlank()) {
-            relatorioDto.setDataFim(null);
-        }
 
         String query = " SELECT a.id as id_vacina, a.nome as nome_vacina, " +
-                " c.nome as bairro_nome, b.data_aplicacao " +
-                " FROM vacina a " +
-                " INNER JOIN vacina_bairro b ON b.vacina_id = a.id " +
-                " INNER JOIN bairro c ON c.id = b.bairro_id " +
-                " WHERE ("+ relatorioDto.getBairro() +" IS NULL OR "+ relatorioDto.getBairro() +" = c.nome)" +
-                " AND ('" + relatorioDto.getDataInicio() + "' IS NULL OR '" + relatorioDto.getDataInicio() + "' > b.data_aplicacao) " +
-                " AND ('" + relatorioDto.getDataFim() + "' IS NULL OR '" + relatorioDto.getDataFim() + "' < b.data_aplicacao) ";
+                    " c.nome as bairro_nome, b.data_aplicacao " +
+                    " FROM vacina a " +
+                    " INNER JOIN vacina_bairro b ON b.vacina_id = a.id " +
+                    " INNER JOIN bairro c ON c.id = b.bairro_id " +
+                    " WHERE ("+ relatorioDto.getBairro() +" IS NULL OR "+ relatorioDto.getBairro() +" = c.nome)";
 
-
-        try{
-        }catch (SQLException e){
-            e.printStackTrace();
-        } finally {
-            conexao.desconectar(conn);
+        if (!relatorioDto.getDataInicio().isBlank()) {
+            query += "AND '" + relatorioDto.getDataInicio() + "' > b.data_aplicacao";
+        }
+        if (!relatorioDto.getDataFim().isBlank()) {
+            query += "AND '" + relatorioDto.getDataFim() + "' > b.data_aplicacao";
         }
 
-        return resultado;
+        Conexao conexao = new Conexao();
+        Connection conn = conexao.conectar();
+
+        PreparedStatement stmt = null;
+        stmt = conn.prepareStatement(query, ResultSet.TYPE_SCROLL_INSENSITIVE, ResultSet.CONCUR_UPDATABLE);
+        ResultSet resultSet = stmt.executeQuery();
+            if (resultSet.wasNull()) {
+                return new ArrayList<>();
+            }
+            while (resultSet.next()) {
+                    vacinas.add(
+                            VacinaBairroDto.builder()
+                                    .id(resultSet.getLong("id_vacina"))
+                                    .bairro(resultSet.getString("bairro_nome"))
+                                    .vacina(resultSet.getString("nome_vacina"))
+                                    .build());
+            }
+
+            conexao.desconectar(conn);
+        return vacinas;
+    }
+
+    public int countAllRows(final ResultSet resultSet) throws SQLException {
+        int count = 0;
+        while (resultSet.next()) {
+            count += 1;
+        }
+        return count+1;
     }
 }
