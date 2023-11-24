ca58ee51d9f8d446e1c160768f291b0e47a07118
kaua.mendes@attornatus.com.br <kaua.mendes@attornatus.com.br>
2023-11-23 21:14:49 -0300

    ajustes

--- src/main/java/com/example/demo/controller/RelatorioController.java
+++ src/main/java/com/example/demo/controller/RelatorioController.java
@@ -1,14 +1,19 @@
 package com.example.demo.controller;
 
+import com.example.demo.domain.Usuario;
 import com.example.demo.dto.RelatorioDto;
 import com.example.demo.repository.RelatorioRepository;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpSession;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.ServletRequestUtils;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
+import java.net.http.HttpRequest;
 import java.sql.SQLException;
 
 @Controller
@@ -24,9 +29,16 @@ public class RelatorioController {
     }
 
     @PostMapping("/buscar")
-    public ModelAndView listar(RelatorioDto relatorioDto) throws SQLException {
-        ModelAndView mv = new ModelAndView("relatorio");
-        mv.addObject("vacinasBairros", relatorioRepository.buscar(relatorioDto));
+    public ModelAndView listar(RelatorioDto relatorioDto, HttpServletRequest request) throws SQLException {
+        ModelAndView mv = new ModelAndView("login");
+        HttpSession session = request.getSession();
+        Usuario usuarioLogado = (Usuario) session.getAttribute("usuarioLogado");
+
+        if (usuarioLogado.isAplicador()) {
+            mv.setViewName("relatorio");
+            mv.addObject("vacinasBairros", relatorioRepository.buscar(relatorioDto));
+            return mv;
+        }
         return mv;
     }
 
--- src/main/java/com/example/demo/services/LoginService.java
+++ src/main/java/com/example/demo/services/LoginService.java
@@ -1,11 +1,13 @@
 package com.example.demo.services;
 
+import jakarta.servlet.http.HttpSession;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.example.demo.domain.Usuario;
 import com.example.demo.dto.LoginDto;
 import com.example.demo.repository.UsuarioRepository;
+import org.springframework.web.servlet.ModelAndView;
 
 @Service
 public class LoginService {
@@ -13,15 +15,21 @@ public class LoginService {
     @Autowired
     private UsuarioRepository usuarioRepository;
 
-    public String findByAccess(LoginDto login) {
+    public ModelAndView findByAccess(LoginDto login, HttpSession session) {
+        ModelAndView mv = new ModelAndView();
         Usuario usuarioLogado = usuarioRepository.findByAccess(login);
         if (usuarioLogado == null) {
-            return "redirect:/";
+            mv.addObject("msgErro", "Usuário não encontrado");
+            mv.setViewName("login");
+            return mv;
         }
         if (usuarioLogado.getCargo().equals(Usuario.TIP_CARGO_APLICADOR)) {
-            return "redirect:/vacinas";
+            session.setAttribute("usuarioLogado", usuarioLogado);
+            mv.setViewName("cadastro_vacina");
+            return mv;
         }
-        return "redirect:/relatorios";
+        mv.setViewName("relatorio");
+        return mv;
     }
 
 }
