c314d669b69b1fc81e0e6a670e6752cb67314776
EduardoDandolini <eduardodasilvadandolini@hmail.com>
2023-11-24 23:14:25 -0300

    refatoração de codigo

--- src/main/java/com/example/demo/services/VacinaService.java
+++ src/main/java/com/example/demo/services/VacinaService.java
@@ -11,18 +11,13 @@ import org.springframework.stereotype.Service;
 public class VacinaService {
     @Autowired
     private VacinaRepository vacinaRepository;
-    @Autowired
-    private Usuario usuario;
+
     public void insert(VacinaDto vacinaDto) throws Exception {
-        if (usuario.getCargo().equals(usuario.TIP_CARGO_APLICADOR)) {
             Vacina vacina = Vacina.builder()
                     .id(Long.valueOf(vacinaDto.getId()))
                     .nome(vacinaDto.getNome())
                     .build();
             vacinaRepository.insert(vacina);
-        } else {
-            throw new Exception("");
-        }
     }
 
     public void findVacinaById(Long id){
