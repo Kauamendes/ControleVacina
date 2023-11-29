ffe3e0d122567a9b1bc5c9aaa81c0775034aa334
KauaMendes <kauamendes80@hotmail.com>
2023-11-29 19:16:55 -0300

    ajustes

--- src/main/java/com/example/demo/controller/VacinaBairroController.java
+++ src/main/java/com/example/demo/controller/VacinaBairroController.java
@@ -1,15 +1,14 @@
 package com.example.demo.controller;
 
 import com.example.demo.domain.Bairro;
+import com.example.demo.dto.VacinaBairroDto;
+import com.example.demo.services.VacinaBairroService;
 import jakarta.servlet.http.HttpSession;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-
-import com.example.demo.dto.VacinaBairroDto;
-import com.example.demo.services.VacinaBairroService;
 import org.springframework.web.servlet.ModelAndView;
 
 import java.sql.SQLException;
@@ -26,16 +25,18 @@ public class VacinaBairroController {
         ModelAndView mv = new ModelAndView("cadastro_vacina");
         mv.addObject("bairros", service.listarBairros ());
         mv.addObject("vacinas", service.listarVacinas());
+        Bairro bairroNaSessao = (Bairro) session.getAttribute("bairroNaSessao");
         String msgSalvar = (String) session.getAttribute("msgSalvar");
         mv.addObject("msgSalvar", msgSalvar);
+        mv.addObject("bairroNaSessao", bairroNaSessao);
         return mv;
     }
 
     @PostMapping
     public String insert(VacinaBairroDto vacinaBairroDto, HttpSession session) throws SQLException {
-        Bairro ultimoBairroSalvo = (Bairro) session.getAttribute("ultimoBairroSalvo");
+        Bairro ultimoBairroSalvo = (Bairro) session.getAttribute("bairroNaSessao");
         if (ultimoBairroSalvo == null || ultimoBairroSalvo.getId() !=  Long.parseLong(vacinaBairroDto.getBairro())) {
-            session.setAttribute("ultimoBairroSalvo", service.buscarBairroPorId(vacinaBairroDto.getBairro()));
+            session.setAttribute("bairroNaSessao", service.buscarBairroPorId(vacinaBairroDto.getBairro()));
         }
         service.insert(vacinaBairroDto);
         session.setAttribute("msgSalvar", "Vacina salva com sucesso!");
