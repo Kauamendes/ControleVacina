bb855ebf8388080b5140c0aa04507193b321fdce
kaua.mendes@attornatus.com.br <kaua.mendes@attornatus.com.br>
2023-12-01 13:00:37 -0300

    ajusttes

--- src/main/java/com/example/demo/controller/RelatorioController.java
+++ src/main/java/com/example/demo/controller/RelatorioController.java
@@ -4,13 +4,16 @@ import com.example.demo.domain.Usuario;
 import com.example.demo.dto.RelatorioDto;
 import com.example.demo.repository.RelatorioRepository;
 import com.example.demo.repository.VacinaBairroRepository;
+import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
+import java.io.IOException;
 import java.sql.SQLException;
 
 import jakarta.servlet.http.HttpSession;
@@ -22,18 +25,24 @@ public class RelatorioController {
     @Autowired
     private RelatorioRepository relatorioRepository;
 
+    @Autowired
+    private VacinaBairroRepository vacinaBairroRepository;
+
     @GetMapping
-    public String relatorio(HttpSession session) {
+    public ModelAndView relatorio(HttpSession session, HttpServletResponse response) throws IOException {
+        ModelAndView mv = new ModelAndView("relatorio");
         String cargo = (String) session.getAttribute("cargo");
         if (cargo.equals(Usuario.TIP_CARGO_APLICADOR)) {
-            return "redirect:/vacinas";
+            response.sendRedirect("/vacinas");
         }
-        return "relatorio";
+        mv.addObject("bairros", vacinaBairroRepository.listarBairros());
+        return mv;
     }
 
     @PostMapping("/buscar")
     public ModelAndView buscar(RelatorioDto relatorioDto) throws SQLException {
         ModelAndView mv = new ModelAndView("relatorio");
+        mv.addObject("bairros", vacinaBairroRepository.listarBairros());
         mv.addObject("vacinasBairros", relatorioRepository.buscar(relatorioDto));
         return mv;
     }
