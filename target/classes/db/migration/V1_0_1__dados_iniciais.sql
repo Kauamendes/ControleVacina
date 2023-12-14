53cfe49c46758adfda832a7a29fc6e2d63f3e8d2
kaua.mendes@attornatus.com.br <kaua.mendes@attornatus.com.br>
2023-12-14 11:02:09 -0300

    Ajustes

--- target/classes/db/migration/V1_0_1__dados_iniciais.sql
+++ target/classes/db/migration/V1_0_1__dados_iniciais.sql
@@ -32,20 +32,45 @@ INSERT INTO BAIRRO (NOME) VALUES ('São Cristóvão');
 INSERT INTO BAIRRO (NOME) VALUES ('Congonhas');
 INSERT INTO BAIRRO (NOME) VALUES ('São Martinho');
 
-INSERT INTO VACINA (NOME, DOENCAS_EVITADAS, DOSAGEM) VALUES ('BCG', 'Proteção contra formas graves da tuberculose (miliar e meníngea)', false);
-INSERT INTO VACINA (NOME, DOENCAS_EVITADAS, DOSAGEM) VALUES ('Hepatite B', 'Proteção contra Hepatite B', true);
-INSERT INTO VACINA (NOME, DOENCAS_EVITADAS, DOSAGEM) VALUES ('Pentavalente (DTP/Hib/Hep B)', 'Proteção contra Difteria, Tétano, Coqueluche, Hepatite B e infecções causadas pelo Haemophilus influenzae B', true);
-INSERT INTO VACINA (NOME, DOENCAS_EVITADAS, DOSAGEM) VALUES ('Pneumocócica 10 valente', 'Proteção contra infecções invasivas (como meningite e pneumonia) e otite média aguda, causadas pelos 10 sorotipos de Streptococus pneumoniae', true);
-INSERT INTO VACINA (NOME, DOENCAS_EVITADAS, DOSAGEM) VALUES ('VIP (Vacina Inativada Poliomielite)', 'Proteção contra Poliomielite', true);
-INSERT INTO VACINA (NOME, DOENCAS_EVITADAS, DOSAGEM) VALUES ('VRH (Vacina Rotavírus Humano)', 'Proteção contra diarreia por rotavírus (Gastroenterites)', true);
-INSERT INTO VACINA (NOME, DOENCAS_EVITADAS, DOSAGEM) VALUES ('Meningocócica C (conjugada)', 'Proteção contra doença invasiva causada pela Neisseria meningitidis do sorogrupo C', true);
-INSERT INTO VACINA (NOME, DOENCAS_EVITADAS, DOSAGEM) VALUES ('Poliomielite 1 e 3 (atenuada) - (VOPb)', 'Proteção contra Poliomielite', true);
-INSERT INTO VACINA (NOME, DOENCAS_EVITADAS, DOSAGEM) VALUES ('Febre Amarela (atenuada) - (FA)', 'Proteção contra Febre Amarela', true);
-INSERT INTO VACINA (NOME, DOENCAS_EVITADAS, DOSAGEM) VALUES ('Tríplice viral (Sarampo, rubéola, caxumba)', 'Proteção contra Sarampo, Caxumba e Rubéola', true);
-INSERT INTO VACINA (NOME, DOENCAS_EVITADAS, DOSAGEM) VALUES ('Tetraviral (Sarampo, rubéola, caxumba, varicela)', 'Proteção contra Sarampo, Caxumba, Rubéola e varicela', false);
-INSERT INTO VACINA (NOME, DOENCAS_EVITADAS, DOSAGEM) VALUES ('Vacina varicela (monovalente)', 'Proteção contra Varicela', false);
-INSERT INTO VACINA (NOME, DOENCAS_EVITADAS, DOSAGEM) VALUES ('Hepatite A (HA - inativada)', 'Proteção contra Hepatite A', false);
-INSERT INTO VACINA (NOME, DOENCAS_EVITADAS, DOSAGEM) VALUES ('DTP (tríplice bacteriana)', 'Proteção contra Difteria, Tétano, Coqueluche', true);
-INSERT INTO VACINA (NOME, DOENCAS_EVITADAS, DOSAGEM) VALUES ('Influenza', 'Proteção contra infecções pelo vírus influenza', true);
-INSERT INTO VACINA (NOME, DOENCAS_EVITADAS, DOSAGEM) VALUES ('Pneumocócica 23-valente - (Pneumo 23)', 'Proteção contra infecções invasivas pelo pneumococo na população indígena', false);
-INSERT INTO VACINA (NOME, DOENCAS_EVITADAS, DOSAGEM) VALUES ('HPV Papilomavírus humano 6, 11, 16 e 18 (HPV4 - recombinante)', 'Proteção contra Papilomavírus Humano 6, 11, 16 e 18', true);
+INSERT INTO VACINA (NOME, DOENCAS_EVITADAS, DOSAGEM)
+VALUES ('BCG', 'Proteção contra formas graves da tuberculose (miliar e meníngea)', false);
+INSERT INTO VACINA (NOME, DOENCAS_EVITADAS, DOSAGEM)
+VALUES ('Hepatite B', 'Proteção contra Hepatite B', true);
+INSERT INTO VACINA (NOME, DOENCAS_EVITADAS, DOSAGEM)
+VALUES ('Pentavalente (DTP/Hib/Hep B)',
+        'Proteção contra Difteria, Tétano, Coqueluche, Hepatite B e infecções causadas pelo Haemophilus influenzae B',
+        true);
+INSERT INTO VACINA (NOME, DOENCAS_EVITADAS, DOSAGEM)
+VALUES ('VIP (Vacina Inativada Poliomielite)', 'Proteção contra Poliomielite', true);
+INSERT INTO VACINA (NOME, DOENCAS_EVITADAS, DOSAGEM)
+VALUES ('Pneumocócica 10 valente',
+        'Proteção contra infecções invasivas (como meningite e pneumonia) e otite média aguda, causadas pelos 10 sorotipos de Streptococus pneumoniae',
+        true);
+INSERT INTO VACINA (NOME, DOENCAS_EVITADAS, DOSAGEM)
+VALUES ('VRH (Vacina Rotavírus Humano)', 'Proteção contra diarreia por rotavírus (Gastroenterites)', true);
+INSERT INTO VACINA (NOME, DOENCAS_EVITADAS, DOSAGEM)
+VALUES ('Meningocócica C (conjugada)',
+        'Proteção contra doença invasiva causada pela Neisseria meningitidis do sorogrupo C', true);
+INSERT INTO VACINA (NOME, DOENCAS_EVITADAS, DOSAGEM)
+VALUES ('Poliomielite 1 e 3 (atenuada) - (VOPb)', 'Proteção contra Poliomielite', true);
+INSERT INTO VACINA (NOME, DOENCAS_EVITADAS, DOSAGEM)
+VALUES ('Febre Amarela (atenuada) - (FA)', 'Proteção contra Febre Amarela', false);
+INSERT INTO VACINA (NOME, DOENCAS_EVITADAS, DOSAGEM)
+VALUES ('Tríplice viral (Sarampo, rubéola, caxumba)', 'Proteção contra Sarampo, Caxumba e Rubéola', true);
+INSERT INTO VACINA (NOME, DOENCAS_EVITADAS, DOSAGEM)
+VALUES ('Tetraviral (Sarampo, rubéola, caxumba, varicela)', 'Proteção contra Sarampo, Caxumba, Rubéola e varicela',
+        false);
+INSERT INTO VACINA (NOME, DOENCAS_EVITADAS, DOSAGEM)
+VALUES ('Vacina varicela (monovalente)', 'Proteção contra Varicela', false);
+INSERT INTO VACINA (NOME, DOENCAS_EVITADAS, DOSAGEM)
+VALUES ('Hepatite A (HA - inativada)', 'Proteção contra Hepatite A', false);
+INSERT INTO VACINA (NOME, DOENCAS_EVITADAS, DOSAGEM)
+VALUES ('DTP (tríplice bacteriana)', 'Proteção contra Difteria, Tétano, Coqueluche', true);
+INSERT INTO VACINA (NOME, DOENCAS_EVITADAS, DOSAGEM)
+VALUES ('Influenza', 'Proteção contra infecções pelo vírus influenza', true);
+INSERT INTO VACINA (NOME, DOENCAS_EVITADAS, DOSAGEM)
+VALUES ('Pneumocócica 23-valente - (Pneumo 23)',
+        'Proteção contra infecções invasivas pelo pneumococo na população indígena', false);
+INSERT INTO VACINA (NOME, DOENCAS_EVITADAS, DOSAGEM)
+VALUES ('HPV Papilomavírus humano 6, 11, 16 e 18 (HPV4 - recombinante)',
+        'Proteção contra Papilomavírus Humano 6, 11, 16 e 18', true);
